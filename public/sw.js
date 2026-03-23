103e808299559a33549af362a7f0cdef566c6ae0
umar khalid <umarkhalidsst@gmail.com>
2026-03-23 14:54:51 +0500

    Add Android PWA support (manifest and service worker)

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "attendance-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "https://cdn.tailwindcss.com",
+  "https://cdn-icons-png.flaticon.com/512/3589/3589030.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
